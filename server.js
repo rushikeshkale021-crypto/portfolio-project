abcf637f07ff79963c43dc751b8d215430b4ee39
rushikeshkale021-crypto <rushikeshkale021@gmail.com>
2026-05-10 16:19:27 +0530

    Added render port

--- server.js
+++ server.js
@@ -22,6 +22,9 @@ app.get("/projects", async (req, res) => {
     res.json(projects);
 });
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
     console.log("Server started");
 });
+
